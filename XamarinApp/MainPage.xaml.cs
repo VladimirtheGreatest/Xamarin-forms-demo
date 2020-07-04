160ec680e60512c25ec9d9ec32fb36cb9ba2304b
Vladimir <vladkodoktorov@gmail.com>
2020-07-04 13:19:09 +0100

    added data acess + basic list view page

--- XamarinApp/MainPage.xaml.cs
+++ XamarinApp/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinApp.Models;
 
 namespace XamarinApp
 {
@@ -13,9 +17,26 @@ namespace XamarinApp
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Girl> girls = new ObservableCollection<Girl>();
+        public ObservableCollection<Girl> Girls { get { return girls; } } 
         public MainPage()
         {
             InitializeComponent();
+            Login();
+        }
+        public async void Login()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("ApiKey", "xrbv1myVu2ZBoHqEBe1odwlIqXVnBkd6YFibJk2q7kVjxrNRuzZpUbGdBJOKlBO");
+            var response = await httpClient.GetStringAsync("https://meetmeapi20200626171452.azurewebsites.net/api/Girls/GetGirls?includeDetails=false");
+            var data = JsonConvert.DeserializeObject<ObservableCollection<Girl>>(response);       
+
+            foreach (var girl in data)
+            {
+                girls.Add(girl);
+            }
+
+            GirlsView.ItemsSource = girls;
         }
     }
 }
---
+++ XamarinApp/Models/Girl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp.Models
+{
+    public class Girl
+    {      
+        public string Nickname { get; set; }    
+        public int Age { get; set; }       
+        public string Bio { get; set; }        
+        public string Picture { get; set; }   
+        public string EyeColor { get; set; }
+    }
+}
